453df2005e42550a289cde19f7dbebff9651e2a8
hanniarojas <hany.cristi@gmail.com>
2023-01-19 13:07:11 -0600

    new update with .yml first github action test

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('./src/collections/Dev/dev-tests.postman_collection.json'),
+    environment: require('./src/collections/Dev/dev-env.postman_environment.json'),
+    reporters: 'cli'
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+});
